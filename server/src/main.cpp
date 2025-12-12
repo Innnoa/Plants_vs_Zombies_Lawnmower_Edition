9cdcd53a8506c5c24731af407d528819b32a785b
innnoa <banxiaqaq@gmail.com>
2025-12-12 17:29:50 +0800

    0.1

---
+++ server/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+int main() {
+    spdlog::info("LawnMower Server 启动中...");
+    spdlog::info("服务端主程序 - 等待实现网络模块");
+    
+    // TODO: 初始化网络服务器
+    // TODO: 启动游戏循环
+    
+    return 0;
+}
